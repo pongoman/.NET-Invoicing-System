fa81bddda91e55bd87fa85b7aadec684df795325
kostas Maniatis <mimousteileis@gmail.com>
2021-09-08 14:44:59 +0200

    invoice controller test

---
+++ XTestController/InvoiceSeedFixture.cs
@@ -0,0 +1,43 @@
+﻿using InvoicingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XTestController
+{
+    public class InvoiceSeedFixture : IDisposable
+    {
+        public static IConfigurationRoot config { get; private set; }
+        public InvoicingSystemContext InvoiceContext { get; private set; } = new InvoicingSystemContext();
+
+        public InvoiceSeedFixture()
+        {
+            var options = new DbContextOptionsBuilder<InvoicingSystemContext>()
+           .UseInMemoryDatabase("assignment_db")
+           .Options;
+
+            InvoiceContext = new InvoicingSystemContext(options, config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                    .Build());
+            InvoiceContext.Invoices.Add(new Invoice
+            {
+                Id = 1,
+                Client = "test",
+                InvoiceAmount = 111
+            });
+            InvoiceContext.Invoices.Add(new Invoice {
+                Id = 2,
+                Client = "test 2",
+                InvoiceAmount = 222
+            });
+            InvoiceContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            InvoiceContext.Dispose();
+        }
+    }
+}
---
+++ XTestController/InvoicesControllerUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using InvoicingSystem.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XTestController
+{
+    public class InvoicesControllerUnitTest : IClassFixture<InvoiceSeedFixture>
+    {
+        readonly InvoiceSeedFixture fixture;
+
+        public InvoicesControllerUnitTest(InvoiceSeedFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void Index_ReturnsAViewResult()
+        {
+            using (var context = fixture.InvoiceContext)
+            {
+                var controller = new InvoicesController(context);
+
+                var result = controller.Index(0, "");
+
+                Assert.IsType<ViewResult>(result);
+            }
+        }
+    }
+}
